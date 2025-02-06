d8c508b20c4a7ec89066274c433a6ef4c506ae4d
The_Kosmo <teregulov31@gmail.com>
2025-02-07 01:30:08 +0500

    1st

---
+++ build/CMakeFiles/helloworld.dir/lib.cpp.obj.d
@@ -0,0 +1,2 @@
+CMakeFiles/helloworld.dir/lib.cpp.obj: E:\Prog\Study_training\lib.cpp \
+ E:\Prog\Study_training\lib.h E:/Prog/Study_training/build/version.h
---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: E:/Prog/Study_training
+# Build directory: E:/Prog/Study_training/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+add_test([=[test_version]=] "test_version")
+set_tests_properties([=[test_version]=] PROPERTIES  _BACKTRACE_TRIPLES "E:/Prog/Study_training/CMakeLists.txt;80;add_test;E:/Prog/Study_training/CMakeLists.txt;0;")
