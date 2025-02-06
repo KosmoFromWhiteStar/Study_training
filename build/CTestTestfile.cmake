07a52d9b03da098880017ced26a454f3e5259cd4
The_Kosmo <teregulov31@gmail.com>
2025-02-07 01:45:35 +0500

    mod

--- build/CTestTestfile.cmake
+++ build/CTestTestfile.cmake
@@ -5,4 +5,4 @@
 # This file includes the relevant testing commands required for 
 # testing this directory and lists subdirectories to be tested as well.
 add_test([=[test_version]=] "test_version")
-set_tests_properties([=[test_version]=] PROPERTIES  _BACKTRACE_TRIPLES "E:/Prog/Study_training/CMakeLists.txt;80;add_test;E:/Prog/Study_training/CMakeLists.txt;0;")
+set_tests_properties([=[test_version]=] PROPERTIES  _BACKTRACE_TRIPLES "E:/Prog/Study_training/CMakeLists.txt;74;add_test;E:/Prog/Study_training/CMakeLists.txt;0;")
